54a82fce85dee74325a0dee2943e0138d2f2a5d2
beniamino sinagra <beniamino.sinagra@gmail.com>
2018-05-10 11:30:57 +0200

    add jsonController and new route

---
+++ app/Http/Controllers/jsonController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 10/05/2018
+ * Time: 11:11
+ */
+
+namespace App\Http\Controllers;
+
+
+class jsonController
+{
+
+}
--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,6 @@ Route::group(['middleware' => 'auth', 'prefix' => '/admin'], function() {
 Route::get('/', 'TorneiController@index');
 Route::post('/store', 'TorneiController@storeData')->name('subscriber.store');
 
+Route::resource ('/prova','jsonController');
+
+
